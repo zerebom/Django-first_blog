8e79d53f2d96360dca40a17809578c9ee725f51c
kokoro <ice.choco.pudding.kokoro@gmail.com>
2019-01-26 14:38:13 +0900

    add migration

--- blog/models.py
+++ blog/models.py
@@ -4,16 +4,25 @@ from django.utils import timezone
 # Create your models here.
 #DBに格納する物は↓のようにmodels.Modelを継承する
 class Post(models.Model):
+    #先ずプロパティを宣言する
+    #他のモデルへのリンク↓
     author=models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    
+    #字数制限付きのテキストフィールド↓
     title=models.CharField(max_length=200)
+    
+    #字数制限なしのテキストフィールド
     text=models.TextField()
+    
+    
+    #日付と時間のフィールド↓
     created_date=models.DateTimeField(
         default=timezone.now
     )
     published_date=models.DateTimeField(
         blank=True,null=True
     )
-
+    #ブログを公開するメソッド↓
     def publish(self):
         self.published_date=timezone.now()
         self.save()
