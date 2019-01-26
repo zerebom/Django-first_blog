6c12e7bbe6800462c75f50170822190196dc9992
kokoro <ice.choco.pudding.kokoro@gmail.com>
2019-01-26 14:27:18 +0900

    second commit

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-
+#DBに格納する物は↓のようにmodels.Modelを継承する
 class Post(models.Model):
     author=models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title=models.CharField(max_length=200)
@@ -10,4 +10,13 @@ class Post(models.Model):
     created_date=models.DateTimeField(
         default=timezone.now
     )
-    published_date
+    published_date=models.DateTimeField(
+        blank=True,null=True
+    )
+
+    def publish(self):
+        self.published_date=timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
